8290cef021f4c37c45bbed7c01911e1e2540ce94
albanionut <ionut.m.alban@gmail.com>
2019-01-09 17:13:38 +0200

    test push

--- src/com/self/Main.java
+++ src/com/self/Main.java
@@ -4,5 +4,9 @@ public class Main {
 
     public static void main(String[] args) {
         System.out.println("Hello Penta!");
+        int a =5;
+        int b =3;
+        int test = a+b;
+        System.out.println(test);
     }
 }
