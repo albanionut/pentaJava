b1819e8b58581fc86ac89d0a9c32ba29d6295da2
albanionut <ionut.m.alban@gmail.com>
2019-01-11 01:18:52 +0200

    Problema1

---
+++ src/com/self/Problema1.java
@@ -0,0 +1,24 @@
+package com.self;
+
+import java.util.Scanner;
+
+public class Problema1 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Suma si aria de la :");
+        int first = sc.nextInt();
+        System.out.println("La:");
+        int last = sc.nextInt();
+        int suma = 0;
+        double count = 0;
+        double medieAritmetica = 1;
+        for (int i = first; i <= last; i++) {
+            suma = suma + i;
+            count++;
+        }
+        medieAritmetica = suma / count;
+        System.out.println("Suma este: " + suma);
+        System.out.println("Aria este: " + medieAritmetica);
+
+    }
+}
