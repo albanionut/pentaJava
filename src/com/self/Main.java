f3ea6298547115b9b9aa27450fcbb0ad0248334a
albanionut <ionut.m.alban@gmail.com>
2019-01-10 23:26:00 +0200

    Test2

--- src/com/self/Main.java
+++ src/com/self/Main.java
@@ -3,10 +3,16 @@ package com.self;
 public class Main {
 
     public static void main(String[] args) {
+        ///Test
         System.out.println("Hello Penta!");
-        int a =5;
-        int b =3;
-        int test = a+b;
+        int a = 5;
+        int b = 3;
+        int test = a + b;
         System.out.println(test);
+        System.out.println("Ana are mere");
+        String d = "Ana are 2 ";
+        String f = "mere";
+        String c = d + f;
+        System.out.println(c);
     }
 }
