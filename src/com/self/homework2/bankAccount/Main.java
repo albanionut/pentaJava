95add485b77a71e168000ad3edb937400616c995
albanionut <ionut.m.alban@gmail.com>
2019-02-19 15:10:01 +0200

    Homework2 Second Exercise

--- src/com/self/homework2/bankAccount/Main.java
+++ src/com/self/homework2/bankAccount/Main.java
@@ -1,6 +1,14 @@
 package com.self.homework2.bankAccount;
 
 public class Main {
+    /*
+    Create a console application from the following requirements:
+    A bank account from where someone has the option to withdraw and deposit money
+    Write information to the console each time someone makes an operation on that account
+    Take into consideration any validations that have to be done since this is a debit account,
+    so user can’t withdraw more than he has into account. Display this information to inform the user about why the
+    operation failed.
+     */
     public static void main(String[] args) {
         Service service = new Service();
         BankAccount client = new BankAccount("Alban Ionut", 300);
