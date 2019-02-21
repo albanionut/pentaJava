bb0c3f0072e1c6851515241e510b39590509e190
albanionut <ionut.m.alban@gmail.com>
2019-02-21 17:45:41 +0200

    HomeWork2 FourthExercise

--- src/com/self/homework2/boardChat/Main.java
+++ src/com/self/homework2/boardChat/Main.java
@@ -1,6 +1,12 @@
 package com.self.homework2.boardChat;
 
 public class Main {
+//    Create a console application from the following requirements:
+//    We have some users that can register into an app with name & email
+//    Users are able to post messages on a board
+//    Each message has a description and an author
+//    The board can display messages from all the users
+
     public static void main(String[] args) {
         Service service = new Service();
         User user1 = new User("Alban","alban@yahoo.com");
